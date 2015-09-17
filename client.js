58c691d1a73b6c0f0aee58877b8f8c57f3582b79
Santiago Gil <gil.educaciontdf@gmail.com>
2015-09-17 09:56:36 -0300

    Evita superposicion de audios.
    El agregar un callback que ejecuta next en la llamada a meSpeak, hace
    que el stream sea pausado hasta que termine de reproducirse el audio.

--- client.js
+++ client.js
@@ -4,9 +4,8 @@ var Writable = require('stream').Writable
 var ws = Writable()
 
 ws._write = function (chunk, enc, next) {
-  console.log(chunk)
-  meSpeak.speak(chunk)
-  process.nextTick(function() {next()})
+  meSpeak.speak(chunk, {}, cb)
+  function cb () { return next() }
 }
 meSpeak.loadVoice(require('mespeak/voices/es-la.json'));
 meSpeak.loadConfig(require('mespeak/src/mespeak_config.json'));
